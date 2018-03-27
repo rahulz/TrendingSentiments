2098e80f22160fc3eac33badf826b9c1d2149c04
Rahul <rahul.k@cubettech.com>
2018-03-27 09:20:25 +0530

    Added keys to settings

--- utils/tweepy/helpers.py
+++ utils/tweepy/helpers.py
@@ -1,17 +1,25 @@
 import pdb
+from enum import Enum
 
 import tweepy
+
+from TrendingSentiments import settings
+
+
+class Places(Enum):
+    INDIA = 23424848
+    WORLDWIDE = 1
 
 
 class TweePy:
     def __init__(self):
-        auth = tweepy.OAuthHandler("ABqdGcPLLERJHc8SuAvWjj15g", "4k6rMnryRLMBKjP9cHZcstViCHpvlgcyBP7WHsNBXHfJotgDXA")
-        auth.set_access_token("569727959-Uo7W00UqRmnFidEpYr62RTsQvSzinRjNFNryxTPz",
-                              "k5X6s4A6RzB005HCY9r9bFqR2OXcwlg0euaao2HDn113H")
+        auth = tweepy.OAuthHandler(settings.TW_CONSUMER_KEY, settings.TW_CONSUMER_SECRET)
+        auth.set_access_token(settings.TW_APP_KEY,
+                              settings.TW_APP_SECRET)
         self.api = tweepy.API(auth)
 
     def get_trends(self):
-        return self.api.trends_place(23424848)
+        return self.api.trends_place(Places.INDIA)
 
     def search(self, q, since_id=None, max_id=None):
         return self.api.search(lang='en', q=q, count=100, result_type='recent', since_id=since_id, max_id=max_id)
